fd6ce6c6daebfa394c39e3ed2de2608b50d49284
Sean <seandrickson@gmail.com>
2013-10-04 10:05:57 -0700

    Updated SCSS comments; Removed all elements from HTML;

--- js/main.js
+++ js/main.js
@@ -3,8 +3,7 @@
   .done(function(icons){
 
     $.each( icons, function(i){
-      ids = ids ? ids + ',' : '';
-      ids += icons[i].id;
+      ids = (ids ? ids+',' : '')+icons[i].id;
     });
 
     $.ajax( 'https://itunes.apple.com/lookup', {
@@ -14,16 +13,20 @@
       data: { id: ids },
     })
     .done(function(data){
+      var $wrapper = $('<div/>')
+        .attr('data-icons', data.resultCount)
+        .addClass('grid__wrapper')
+        .appendTo('body');
       $.each(data.results, function(i){
         $('<div/>')
-          .attr('data-icons', data.resultCount)
           .addClass('grid__item icon__wrapper')
           .hide()
-          .append('<a href="' + icons[i].url + '" class="icon__item">' +
-                  '<img alt="' + icons[i].name + '" src="' + data.results[i].artworkUrl100 + '">' +
-                  '</a>')
-          .appendTo('#icons')
-          .delay(200 * i)
+          .append(
+            '<a href="'+icons[i].url+'" class="icon__item">'+
+            '<img alt="'+icons[i].name+'" src="'+data.results[i].artworkUrl100+'">'+
+            '</a>')
+          .appendTo($wrapper)
+          .delay(200*i)
           .fadeIn();
       });
     });
